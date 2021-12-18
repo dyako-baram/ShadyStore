6796516df11177e1351ea62f3d64e398ffb72a1c
Dyako Baram <dyako.baram@gmail.com>
2021-12-18 11:37:39 +0300

    finishing contact page

--- public/js/app.js
+++ public/js/app.js
@@ -10,9 +10,14 @@ function getCookie(name) {
   if (parts.length === 2) return parts.pop().split(";").shift();
 }
 const isSigned = document.querySelector("#login_signed");
+const account = document.querySelector("#nav_link_account");
+
 const cEmail = getCookie("email");
 if (cEmail != "") {
   isSigned.innerText = `Logout`;
+  isSigned.title="Logout"
+  isSigned.href="logout.html"
+
   document.querySelector('#myEmail').innerText=`Welcome ${cEmail}`
 } else {
   isSigned.innerText = "login/signup";
