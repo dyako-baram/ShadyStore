57a6597e29ada77502f943510ff3e83079647133
Dyako Baram <dyako.baram@gmail.com>
2022-01-24 20:44:05 +0300

    minor bugs fixs

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,11 @@ const nav = document.getElementsByClassName("links")[0];
 toggle.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
-
+const imgs = document.querySelectorAll("img");
+for (let index = 0; index < imgs.length; index++) {
+  imgs[index].setAttribute("loading", "lazy");
+  imgs[index].removeAttribute("lazy");
+}
 function getCookie(name) {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
@@ -13,12 +17,12 @@ const isSigned = document.querySelector("#login_signed");
 const account = document.querySelector("#nav_link_account");
 
 const cEmail = getCookie("email");
-if (cEmail != "" && cEmail!=null) {
+if (cEmail != "" && cEmail != null) {
   isSigned.innerText = `Logout`;
-  isSigned.title="Logout"
-  isSigned.href="logout.html"
+  isSigned.title = "Logout";
+  isSigned.href = "logout.html";
 
-  document.querySelector('#myEmail').innerText=`Welcome ${cEmail}`
+  document.querySelector("#myEmail").innerText = `Welcome ${cEmail}`;
 } else {
   isSigned.innerText = "login/signup";
 }
